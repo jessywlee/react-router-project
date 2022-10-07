252a6c8c5158397e6f8179b00b40f92b94edc4a6
yewon.lee <yewon.lee@seavantage.com>
2022-10-07 16:47:28 +0900

    inital commit

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h1>root</h1>
+      <button>go main</button>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Root.jsx
@@ -0,0 +1,10 @@
+const Root = () => {
+  return (
+    <>
+      <h1>root</h1>
+      <button>about</button>
+    </>
+  );
+}
+
+export default Root;
